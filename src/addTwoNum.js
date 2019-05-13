b94c1bfa2b15ce7964915031a362c99dae4f2c91
Pigrabbit <donghyuk1003@gmail.com>
2019-05-13 23:58:21 +0900

    Update addTwoNum solution to recursive function

--- src/addTwoNum.js
+++ src/addTwoNum.js
@@ -22,47 +22,31 @@ class ListNode {
 }
 
 const addTwoNumbers = (list1, list2) => {
-    // refactor it to recursive call
-    let resultNode = new ListNode(0)
+    if (list1.next && list2.next) {
+        addTwoNumbers(list1.next, list2.next)
+    } else if (list1.next && !list2.next) {
 
-    let add = 0
-    let sum = list1.val + list2.val
-    if (sum >= 10) {
-        sum = sum % 10
-        add++
-    }
+    } else if (!list1.next && list2.next) {
 
-    resultNode.val = sum
-    resultNode.next = new ListNode(add)
-    let currentNode = resultNode.next
-
-    while(list1.next && list2.next) {
-        list1 = list1.next
-        list2 = list2.next
-
-        let add = 0
-        let sum = list1.val + list2.val + currentNode.val
+    } else {
+        let sum = list1.val + list2.val
+        let resultNode = new ListNode()
         if (sum >= 10) {
             sum = sum % 10
-            add++
+            resultNode.val = sum
+            resultNode.next = new ListNode(1)
+        } else {
+            resultNode.val = sum
         }
 
-        currentNode.val = sum
-        currentNode.next = new ListNode(add)
-        currentNode = currentNode.next
+        return resultNode
     }
-
-    return resultNode
 }
 
 let list1 = new ListNode(9)
-list1.next = new ListNode(4)
-list1.next.next = new ListNode(4)
 console.log(list1)
 
 let list2 = new ListNode(3)
-list2.next = new ListNode(5)
-list2.next.next = new ListNode(8)
 console.log(list2)
 
 console.log(addTwoNumbers(list1, list2))
