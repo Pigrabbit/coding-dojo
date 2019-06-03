a2f72ba6e3c1afc11d90555b256c1706adead74f
Pigrabbit <donghyuk1003@gmail.com>
2019-06-03 13:04:29 +0900

    Add solution for find k-th largest element in array
    Problem: https://leetcode.com/problems/kth-largest-element-in-an-array/

---
+++ test/findKthLargest.test.js
@@ -0,0 +1,24 @@
+const findKthLargest = require('../src/findKthLargest')
+
+describe('findKthLargest', () => {
+    describe('with non duplicate element', () => {
+        const inputArray = [3, 2, 1, 5, 6, 4]
+        it('returns k-th largest element', () => {
+            expect(findKthLargest(inputArray, 2)).toEqual(5)
+        })
+    })
+
+    describe('with duplicate element', () => {
+        const inputArray = [3,2,3,1,2,4,5,5,6]
+        it('returns k-th largest element', () => {
+            expect(findKthLargest(inputArray, 4)).toEqual(4)
+        })
+    })
+
+    describe('with multiple duplicate element', () => {
+        const inputArray = [3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
+        it('returns k-th largest element', () => {
+            expect(findKthLargest(inputArray, 2)).toEqual(10)
+        })
+    })
+})
