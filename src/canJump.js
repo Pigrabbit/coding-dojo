2b05f2f275a3b09e88f4572abc45453c662109e3
Pigrabbit <donghyuk1003@gmail.com>
2019-07-05 13:41:18 +0900

    Add solution for jump game algorithm
    Problem: https://leetcode.com/problems/jump-game/

---
+++ src/canJump.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = nums => {
+    let maxJumpDistance = nums[0]
+
+    for (let index = 1; index < nums.length; index++) {
+        if (maxJumpDistance < index) {
+            return false
+        }
+
+        maxJumpDistance = Math.max(maxJumpDistance, nums[index] + index)
+    }
+
+    return true
+}
+
+module.exports = canJump
