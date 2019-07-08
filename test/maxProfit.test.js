0d7059a1d22212980c18c131fc0c1e90b523c422
Jason Kang <donghyuk1003@gmail.com>
2019-07-08 18:12:33 +0900

    Add solution for best time to sell and buy stock algorithm
    Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ test/maxProfit.test.js
@@ -0,0 +1,19 @@
+const maxProfit = require('../src/maxProfit')
+
+describe('maxProfit', () => {
+    describe('Given an array for which the ith element is the price of a given stock on day i', () => {
+        const prices = [7, 1, 5, 3, 6, 4]
+
+        it('finds the maximum profit', () => {
+            expect(maxProfit(prices)).toEqual(5)
+        })
+    })
+
+    describe('Given an array with descending prices', () => {
+        const prices = [7, 6, 4, 3, 1]
+
+        it('return 0', () => {
+            expect(maxProfit(prices)).toEqual(0)
+        })
+    })
+})
