ccebf552bc4ece33732756ac5e0e4044663976d6
Jason Kang <donghyuk1003@gmail.com>
2019-07-12 11:09:00 +0900

    Add solution for minimum coin change algorithm
    Problem: https://leetcode.com/problems/coin-change/

---
+++ src/coinChange.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = (coins, amount) => {
+    const numCoins = new Array(amount + 1)
+    numCoins.fill(Number.MAX_SAFE_INTEGER)
+    numCoins[0] = 0
+
+    for (let amountIter = 1; amountIter <= amount; amountIter++) {
+        for (let coinsIter = 0; coinsIter <coins.length; coinsIter++) {
+            if (amountIter - coins[coinsIter] >= 0) {
+                numCoins[amountIter] = Math.min(numCoins[amountIter - coins[coinsIter]] + 1, numCoins[amountIter])
+            }
+            console.log(numCoins)
+        }
+    }
+
+    return numCoins[amount] === Number.MAX_SAFE_INTEGER ? -1 : numCoins[amount]
+}
+
+module.exports = coinChange
