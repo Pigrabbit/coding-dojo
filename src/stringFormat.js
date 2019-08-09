0ca61d6bcb129f41710296b96fb12df8cbfb4fa0
Pigrabbit <donghyuk1003@gmail.com>
2019-08-09 17:15:04 +0900

    Implement stringFormat function

---
+++ src/stringFormat.js
@@ -0,0 +1,32 @@
+const stringFormat = (formatString, args) => {
+  let resultString = formatString
+
+  if (!isConvertable(formatString, args)) {
+    return 'Number of arguments and positions do not match'
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    const reg = new RegExp('\{([' + i.toString() + '])\}', 'g')
+    resultString = resultString.replace(reg, args[i])
+  }
+
+  return resultString
+}
+
+const isConvertable = (formatString, args) => {
+  const indices = []
+  let match = []
+  const regex = /\{([^}+])\}/g
+
+  while ((match = regex.exec(formatString)) !== null) {
+    indices.push(match.index)
+  }
+
+  if (indices.length !== args.length) {
+    return false
+  }
+
+  return true
+}
+
+module.exports = stringFormat
